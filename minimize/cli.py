b641f2911a044484496521c7cdc8ab65d2f5b7b9
TSNoble <tom.noble@bjss.com>
2021-04-13 22:20:20 +0100

    Remove whitespace

--- minimize/cli.py
+++ minimize/cli.py
@@ -10,7 +10,6 @@ LINE_LENGTH = 80
 @click.command()
 @click.argument("globs", nargs=-1)
 def minimize(globs):
-
     globs = globs or ("./**/*",)
     files = get_py_files(globs)
     print("=" * LINE_LENGTH)
