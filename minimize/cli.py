69a9e10c803a6eae29ffd6f748ebfbe626c384b4
TSNoble <tom.noble@bjss.com>
2021-04-13 22:19:35 +0100

    Add info printing to minimize command

--- minimize/cli.py
+++ minimize/cli.py
@@ -1,11 +1,25 @@
+import os
+
 import click
+
+from minimize.util import get_py_files, file_processed_message
+
+LINE_LENGTH = 80
 
 
 @click.command()
 @click.argument("globs", nargs=-1)
 def minimize(globs):
-    globs = globs or "./**"
-    print(f"Minimizing {globs}")
+
+    globs = globs or ("./**/*",)
+    files = get_py_files(globs)
+    print("=" * LINE_LENGTH)
+    for file in files:
+        size_bytes = os.path.getsize(file)
+        print(file_processed_message(file, size_bytes, size_bytes, LINE_LENGTH))
+    print("-" * LINE_LENGTH)
+    print("All done! \N{left-pointing magnifying glass}")
+    print("=" * LINE_LENGTH)
 
 
 if __name__ == "__main__":
