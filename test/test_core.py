12ddbec558e75642a1a82827054b91e24c64e6b2
TSNoble <tom.noble@bjss.com>
2021-04-15 21:14:17 +0100

    Add unit tests

---
+++ test/test_core.py
@@ -0,0 +1,15 @@
+from minimize.core import strip_comments, strip_extra_newlines, strip_trailing_whitespace
+
+
+def test_strip_comments():
+    assert strip_comments(b"#foo\nbar") == b"\nbar"
+    assert strip_comments(b"bar #foo") == b"bar "
+
+
+def test_strip_extra_newlines():
+    assert strip_extra_newlines(b"foo\n\n\n") == b"foo\n"
+
+
+def test_strip_trailing_whitespace():
+    assert strip_trailing_whitespace(b"foo     \n") == b"foo\n"
+    assert strip_trailing_whitespace(b"foo\t\t\t\n") == b"foo\n"
